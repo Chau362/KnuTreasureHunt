6a50c326c523cd404fb1945aece4e9677ea80045
BG <bogu5407@nate.com>
2017-04-04 17:04:17 +0300

    Login@@@

--- app/src/main/java/pearsistent/knutreasurehunt/Login_admin.java
+++ app/src/main/java/pearsistent/knutreasurehunt/Login_admin.java
@@ -2,29 +2,63 @@ package pearsistent.knutreasurehunt;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
-public class Login_admin extends AppCompatActivity {
-    EditText Txt_user;
-    EditText Txt_pwd;
-    Button Login;
-    Button Register;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class Login_admin extends BaseActivity {
+    private static final String TAG = "LOGIN_ADMIN";
+
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+
+    private EditText Txt_user;
+    private EditText Txt_pwd;
+    private Button Login;
+    private Button Register;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_admin);
+
         Txt_user= (EditText) findViewById(R.id.login_admin_usr);
         Txt_pwd= (EditText) findViewById(R.id.login_admin_pwd);
         Login = (Button) findViewById(R.id.Btn_login_admin_Login);
         Register = (Button) findViewById(R.id.Btn_login_admin_Register);
+
+        mAuth = FirebaseAuth.getInstance();
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
+                } else {
+                    // User is signed out
+                    Log.d(TAG, "onAuthStateChanged:signed_out");
+                }
+                // ...
+            }
+        };
+
         Login.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String user = Txt_user.getText().toString();
                 String pwd = Txt_pwd.getText().toString();
+                signIn(user,pwd);
             }
         });
         Register.setOnClickListener(new View.OnClickListener() {
@@ -34,5 +68,53 @@ public class Login_admin extends AppCompatActivity {
                 startActivity(i);
             }
         });
+    }
+    @Override
+    public void onStart() {
+        super.onStart();
+        mAuth.addAuthStateListener(mAuthListener);
+    }
+    @Override
+    public void onStop() {
+        super.onStop();
+        if (mAuthListener != null) {
+            mAuth.removeAuthStateListener(mAuthListener);
+        }
+    }
+    private void signIn(String email, String password) {
+        Log.d(TAG, "signIn:" + email);
+        //if (!validateForm()) {
+        //    return;
+        //}
+
+        showProgressDialog();
+
+        // [START sign_in_with_email]
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
+                        Toast.makeText(Login_admin.this, "Success!",
+                                Toast.LENGTH_SHORT).show();
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        if (!task.isSuccessful()) {
+                            Log.w(TAG, "signInWithEmail:failed", task.getException());
+                            Toast.makeText(Login_admin.this, R.string.auth_failed,
+                                    Toast.LENGTH_SHORT).show();
+                        }
+
+                        // [START_EXCLUDE]
+                        if (!task.isSuccessful()) {
+                            //mStatusTextView.setText(R.string.auth_failed);
+                            Log.d(TAG,"FAILED");
+                        }
+                        hideProgressDialog();
+                        // [END_EXCLUDE]
+                    }
+                });
+        // [END sign_in_with_email]
     }
 }
