c31bf332be3f775670d832df75f27a6ce1d65327
BG <bogu5407@nate.com>
2017-04-15 13:41:13 +0300

    add Page modify graddle for login problem add permission to manifest add Map(not current position)

--- app/src/main/java/pearsistent/knutreasurehunt/MainActivity.java
+++ app/src/main/java/pearsistent/knutreasurehunt/MainActivity.java
@@ -29,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
     /**
      * The {@link ViewPager} that will host the section contents.
      */
+    //edited by Bogyu 4.15, add page
     private ViewPager mViewPager;
 
     @Override
@@ -135,23 +136,30 @@ public class MainActivity extends AppCompatActivity {
                 bundle = new Bundle();
                 //Log.i("eee","tab2");
             }
+            if(position == 2){
+                fragment = new Usermap();
+                bundle = new Bundle();
+
+            }
 
             return fragment;
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 2;
+            // Show 4 total pages.
+            return 3;
         }
 
         @Override
         public CharSequence getPageTitle(int position) {
             switch (position) {
                 case 0:
-                    return "SECTION 1";
+                    return "TAB 1";
                 case 1:
-                    return "SECTION 2";
+                    return "TAB 2";
+                case 2:
+                    return "TAB 3";
             }
             return null;
         }
