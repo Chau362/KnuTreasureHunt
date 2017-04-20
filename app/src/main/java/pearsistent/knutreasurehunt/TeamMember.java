953c17d5a19a61e3f71b36c6696977c6421cf14e
BG <bogu5407@nate.com>
2017-04-20 16:30:55 +0300

    User can get map and current location. I should compose more function. (when location is changed)

--- app/src/main/java/pearsistent/knutreasurehunt/TeamMember.java
+++ app/src/main/java/pearsistent/knutreasurehunt/TeamMember.java
@@ -4,8 +4,14 @@ package pearsistent.knutreasurehunt;
  * Created by Zzeulki on 2017. 3. 30..
  */
 
+
 public class TeamMember {
+
     String memberName;
+
+    public TeamMember(){
+
+    }
 
     public void setMemberName(String name){
         this.memberName = name;
