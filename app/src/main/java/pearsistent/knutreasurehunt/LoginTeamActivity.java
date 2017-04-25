2c04ee169d63e1f8619b4939938c99708706dfb2
timbrowning <c14519547@mydit.ie>
2017-04-25 13:27:42 +0900

    Merge branch 'master' of /Users/timbrowning/Desktop/Knu/KnuTreasureHunt with conflicts.

--- app/src/main/java/pearsistent/knutreasurehunt/LoginTeamActivity.java
+++ app/src/main/java/pearsistent/knutreasurehunt/LoginTeamActivity.java
@@ -1,27 +1,13 @@
 package pearsistent.knutreasurehunt;
 
 import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.support.annotation.NonNull;
-import android.text.method.PasswordTransformationMethod;
-import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.Toast;
 
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
-import com.google.firebase.auth.AuthResult;
-import com.google.firebase.auth.FirebaseAuth;
-import com.google.firebase.auth.FirebaseUser;
-
-public class LoginTeamActivity extends BaseActivity{
-    private static final String TAG = "LOGIN_TEAM_USER";
-
-    private FirebaseAuth mAuth;
-    private FirebaseAuth.AuthStateListener mAuthListener;
-
+public class LoginTeamActivity extends AppCompatActivity {
     EditText userName;
     EditText userPwd;
     Button loginBtn;
@@ -37,34 +23,15 @@ public class LoginTeamActivity extends BaseActivity{
         loginBtn = (Button) findViewById(R.id.Btn_login_team_Login);
         registerBtn = (Button) findViewById(R.id.Btn_login_team_Register);
 
-        //hide type password
-        userPwd.setTransformationMethod(PasswordTransformationMethod.getInstance());
-
-
-        mAuth = FirebaseAuth.getInstance();
-        mAuthListener = new FirebaseAuth.AuthStateListener() {
-            @Override
-            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
-                FirebaseUser user = firebaseAuth.getCurrentUser();
-                if (user != null) {
-                    // User is signed in
-                    Log.d(TAG, "onAuthStateChanged:signed_in:" + user.getUid());
-                } else {
-                    // User is signed out
-                    Log.d(TAG, "onAuthStateChanged:signed__out");
-                }
-                ///// ...
-            }
-        };
-
         loginBtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 String user = userName.getText().toString();
                 String pwd = userPwd.getText().toString();
 
-                signIn(user,pwd);
-
+                //2017.04.03 seulki : If you complete login function, you can use it.
+                //Intent i = new Intent(getApplicationContext(),MainActivity.class);
+                //startActivity(i);
             }
         });
         registerBtn.setOnClickListener(new View.OnClickListener() {
@@ -75,61 +42,4 @@ public class LoginTeamActivity extends BaseActivity{
             }
         });
     }
-
-    @Override
-    public void onStart() {
-        super.onStart();
-        mAuth.addAuthStateListener(mAuthListener);
-    }
-    @Override
-    public void onStop() {
-        super.onStop();
-        if (mAuthListener != null) {
-            mAuth.removeAuthStateListener(mAuthListener);
-        }
-    }
-    private void signIn(String email, String password) {
-
-        Log.d(TAG, "signIn:" + email);
-        //if (!validateForm()) {
-        //    return;
-        //}
-
-        showProgressDialog();
-
-        // [START sign_in_with_email]
-        mAuth.signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
-
-                    @Override
-                    public void onComplete(@NonNull Task<AuthResult> task) {
-
-                        if(task.isSuccessful()) {
-                            Log.d(TAG, "signInWithEmail:onComplete:" + task.isSuccessful());
-                            Toast.makeText(LoginTeamActivity.this, "Success!",
-                                    Toast.LENGTH_SHORT).show();
-
-                            //2017.04.03 seulki : If you complete login function, you can use it.
-                            Intent i = new Intent(getApplicationContext(),MainActivity.class);
-                            startActivity(i);
-
-
-                        }
-                        // If sign in fails, display a message to the user. If sign in succeeds
-                        // the auth state listener will be notified and logic to handle the
-                        // signed in user can be handled in the listener.
-                        else if (!task.isSuccessful()) {
-                            Log.w(TAG, "signInWithEmail:failed", task.getException());
-                            Toast.makeText(LoginTeamActivity.this, R.string.auth_failed,
-                                    Toast.LENGTH_SHORT).show();
-
-                        }
-                        hideProgressDialog();
-                        // [END_EXCLUDE]
-
-                    }
-                });
-        // [END sign_in_with_email]
-    }
 }
-
