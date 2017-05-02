2a1ec981121ad9195f4067146599149f0ac2c3b0
BG <bogu5407@nate.com>
2017-05-03 01:45:03 +0300

    Get team from db

--- app/src/main/java/pearsistent/knutreasurehunt/CreatePDF.java
+++ app/src/main/java/pearsistent/knutreasurehunt/CreatePDF.java
@@ -1,13 +1,26 @@
 package pearsistent.knutreasurehunt;
 
 
+import android.content.Intent;
 import android.net.Uri;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
 import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseException;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FileDownloadTask;
 import com.google.firebase.storage.FirebaseStorage;
 import com.google.firebase.storage.StorageReference;
 import com.google.firebase.storage.UploadTask;
@@ -19,47 +32,78 @@ import com.itextpdf.text.pdf.PdfWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
+import java.io.IOException;
 import java.io.OutputStream;
+import java.util.ArrayList;
 
 public class CreatePDF extends AppCompatActivity {
 
     private OutputStream output;
     private FirebaseStorage storage;
-    private StorageReference storageRef;
+    private StorageReference storageRef,pathRef;
+    private DatabaseReference mDatabase;
+
     private File pdfFolder;
     private File myFile;
+
+    private String imagePath;
+    private ArrayList<Team> teamList=null;
+    private String teamName;
+
+    private int itemPoint;
+    private  ArrayList<Item> teamItemListNames;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_createpdf);
 
+        mDatabase = FirebaseDatabase.getInstance().getReferenceFromUrl("https://treasurehunt-5d55f.firebaseio.com/");
         storage = FirebaseStorage.getInstance();
         storageRef = storage.getReferenceFromUrl("gs://treasurehunt-5d55f.appspot.com");
-        Log.d("CreatePDF","success open");
+
+        // Read from the database
+        mDatabase.child("Team").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                ///팀을 받아와야 한다..
+                teamList = new ArrayList<Team>();
+                Log.d("DB", "Cheer up " );
+
+                for (DataSnapshot teamSnapshot : dataSnapshot.getChildren()) {
+                    Team currentTeam = teamSnapshot.getValue(Team.class);
+
+                    teamList.add(currentTeam);
+                    Log.d("TeamList",""+currentTeam.getTeamName());
+                }
+            }
+
+            @Override
+            public void onCancelled(DatabaseError error) {
+                // Failed to read value
+                Log.w("DB", "Failed to read value.", error.toException());
+            }
+        });
+
+
+        Log.d("CreatePDF","open success");
         try {
             createPath();
             createPDF();
             uploadFile();
-            Toast.makeText(this, "Success!",
+                    Toast.makeText(this, "Success!",
                     Toast.LENGTH_SHORT).show();
             //viewPdf();
         } catch (FileNotFoundException e) {
             e.printStackTrace();
         } catch (DocumentException e) {
             e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
         }
     }
 
-    //this version is static. so you have to change it to work dynamically
-    private void uploadFile() {
-        final StorageReference childRef = storageRef.child("TeamA/Doc1.pdf");
-        Uri objectURI = FileProvider.getUriForFile(getApplicationContext(),getApplicationContext().getPackageName()+".provider",myFile);
-        UploadTask uploadTask = childRef.putFile(objectURI);
-    }
-
     public void createPath() throws FileNotFoundException {
-
         pdfFolder = new File("sdcard/TreasureHunt_PDF");
         if (!pdfFolder.exists()) {
             pdfFolder.mkdir();
@@ -70,7 +114,7 @@ public class CreatePDF extends AppCompatActivity {
     }
 
     // @TargetApi(Build.VERSION_CODES.KITKAT)
-    public void createPDF() throws DocumentException {
+    public void createPDF() throws DocumentException, IOException {
         //Step 1
         Document document = new Document();
 
@@ -80,15 +124,49 @@ public class CreatePDF extends AppCompatActivity {
         //Step 3
         document.open();
 
-        //Step 4 Add content
-//        document.add(new Paragraph(mSubjectEditText.getText().toString()));
-//        document.add(new Paragraph(mBodyEditText.getText().toString()));
+        //Step 4 get file from Firebase
 
+        /*
+        File localFile = File.createTempFile("images", "jpg");
+
+
+        storageRef.child("TeamA").getFile(localFile)
+                .addOnSuccessListener(new OnSuccessListener<FileDownloadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(FileDownloadTask.TaskSnapshot taskSnapshot) {
+                        // Successfully downloaded data to local file
+                        // ...
+                        Log.d("getFile","File created");
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception exception) {
+                // Handle failed download
+                // ...
+                Log.d("getFile","Error");
+            }
+        });*/
         document.add(new Paragraph("test"));
         document.add(new Paragraph("Testttt"));
 
         //Step 5: Close the document
         document.close();
     }
+    //this version is static. so you have to change it to work dynamically
+    private void uploadFile() {
+        final StorageReference childRef = storageRef.child("TeamA/Doc1.pdf");
+        Uri objectURI = FileProvider.getUriForFile(getApplicationContext(),getApplicationContext().getPackageName()+".provider",myFile);
+        UploadTask uploadTask = childRef.putFile(objectURI);
+    }
 
+    private StorageReference findImageFile(String imageFileName) {
+
+        if (teamName != null) {
+            //can make a image file name
+            StorageReference childRef = storageRef.child(teamName + "/" + imageFileName);
+            return childRef;
+        }
+
+        return null;
+    }
 }
